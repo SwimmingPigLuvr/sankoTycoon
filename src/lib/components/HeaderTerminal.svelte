4153ae783a618b7bdb6e3a02cc30af7117eff2e6
SwimmingPigLuvr <dnevell95@gmail.com>
2024-09-29 00:49:56 -0700

    Updated files: src/lib/components/HeaderTerminal.svelte

--- src/lib/components/HeaderTerminal.svelte
+++ src/lib/components/HeaderTerminal.svelte
@@ -1,17 +1,19 @@
 <script>
-	let messages = [];
+	let messages = ['', '', '',''];
 	let newMessage = 'bridge to sanko';
 </script>
 
 <main class="bg-black text-white font-mono -tracking-widest p-2 w-full">
-	{#each messages as message}
-		<p class="text-slate-800">
-			. {message}
+	<div class="w-full flex flex-col justify-end">
+		{#each messages as message}
+			<p class="text-slate-600">
+				. {message}
+			</p>
+		{/each}
+		<p class="slate-200">
+			> {newMessage} <span class="pulsate">|</span>
 		</p>
-	{/each}
-	<p class="slate-200">
-		> {newMessage} <span class="pulsate text-white">|</span>
-	</p>
+	</div>
 </main>
 
 <style>
@@ -20,6 +22,6 @@
 		animation-iteration-count: infinite;
 		-webkit-animation: pulsate 0.5s ease-out;
 		-webkit-animation-iteration-count: infinite;
-		opacity: 0;
+		opacity: 100;
 	}
 </style>
