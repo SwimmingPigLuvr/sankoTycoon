5d9a9ac29145645a660f94d773b406d0947bbad9
SwimmingPigLuvr <dnevell95@gmail.com>
2024-09-29 00:43:31 -0700

    Updated files: src/routes/+layout.svelte src/routes/+page.svelte

---
+++ src/lib/components/HeaderTerminal.svelte
@@ -0,0 +1,25 @@
+<script>
+	let messages = [];
+	let newMessage = 'bridge to sanko';
+</script>
+
+<main class="bg-black text-white font-mono -tracking-widest p-2 w-full">
+	{#each messages as message}
+		<p class="text-slate-800">
+			. {message}
+		</p>
+	{/each}
+	<p class="slate-200">
+		> {newMessage} <span class="pulsate text-white">|</span>
+	</p>
+</main>
+
+<style>
+	.pulsate {
+		animation: pulsate 0.5s ease-out;
+		animation-iteration-count: infinite;
+		-webkit-animation: pulsate 0.5s ease-out;
+		-webkit-animation-iteration-count: infinite;
+		opacity: 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import HeaderTerminal from '../lib/components/HeaderTerminal.svelte';
+</script>
+
+<main class="p-2">
+		<h1 class=" w-full text-center">Sanko Tycoon ©️</h1>
+	<HeaderTerminal />
+	<p class="text-fuchsia-50">
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</main>
