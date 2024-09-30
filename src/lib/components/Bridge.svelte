67d054f4824f017bed3ca611e1d70888b33bfebb
SwimmingPigLuvr <dnevell95@gmail.com>
2024-09-30 07:34:42 -0700

    Updated files: src/lib/components/Bridge.svelte

--- src/lib/components/Bridge.svelte
+++ src/lib/components/Bridge.svelte
@@ -1,13 +1,19 @@
 <script>
-	function bridgeToSanko() {
+	function bridgeToSanko() {}
 
+	function handleHoverBridge() {
+		const audio = new Audio('hover.wav');
+		audio.play();
 	}
 </script>
 
 <main class="">
 	<div>
-		<button 
-			class="rounded-lg text-sm p-1 border-black border-2 px-2 bg-gradient-to-b from-white to-slate-400 hover:border-slate-300" 
-			on:click={bridgeToSanko}>Bridge to Sanko</button>
+		<button
+			class="rounded-lg text-sm p-1 border-black border-2 px-2 bg-gradient-to-b from-white to-slate-400 hover:border-slate-300"
+			on:focus={handleHoverBridge}
+			on:mouseover={handleHoverBridge}
+			on:click={bridgeToSanko}>Bridge to Sanko</button
+		>
 	</div>
 </main>
