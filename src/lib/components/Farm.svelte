869e9a2def50550ab0e437bce59f94d4a3966a7b
SwimmingPigLuvr <dnevell95@gmail.com>
2024-10-03 13:55:42 -0700

    Updated files: src/lib/components/Farm.svelte

--- src/lib/components/Farm.svelte
+++ src/lib/components/Farm.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { wallet, type Bun } from '$lib/stores/wallet';
+	import { get } from 'svelte/store';
 	import Wallet from './Wallet.svelte';
 
 	export let bun: Bun;
@@ -11,6 +12,32 @@
 	function selectPlot(index: number) {
 		if (plots[index] === 'empty') {
 			selectedPlotIndex = index;
+		} else {
+			selectedPlotIndex = null;
+		}
+	}
+
+	// Function to plant a seed in the selected plot
+	function plantSeed() {
+		if (selectedPlotIndex !== null) {
+			const availableSeeds = get(wallet).nfts.find((nft) => nft.id === bun.id)?.wallet.seeds;
+
+			if (availableSeeds && availableSeeds.round > 0) {
+				// Reduce the seed count
+				wallet.update((currentWallet) => {
+					const bunIndex = currentWallet.nfts.findIndex((nft) => nft.id === bun.id);
+					if (bunIndex !== -1) {
+						currentWallet.nfts[bunIndex].wallet.seeds.round -= 1;
+					}
+					return currentWallet;
+				});
+
+				// Update the plot state
+				plots[selectedPlotIndex] = 'planted';
+				selectedPlotIndex = null; // Deselect plot after planting
+			} else {
+				alert('You do not have enough seeds to plant!');
+			}
 		}
 	}
 
@@ -18,32 +45,45 @@
 	// if the user has seeds then they will be prompted to plant them their
 	// impl a planting function
 	// planting takes the seed and plants a tree in the plot
+	// todo. farm needs to be rotated. let me clarify:
+	// plot 1 is the bottom right grid
+	// plot 5 is the top right grid
+	// plot 21 is the bottom left grid
+	// plot 25 is the top left
+
+	// right now the plant seed function only deals with the round seeds
+	// but we need to only consider the types that exist in the wallet
+	// could be any of the five types or none
+	// if seeds, the div that says plant seeds in plot should say
+	// empty plot, then have the available seeds images from left to right
+	// then a button below that says plant seeds
+	// if no seeds, should just be empty
 </script>
 
 <div class="grid gap-0 grid-cols-5 grid-rows-5 w-40 h-40 border-black border-2">
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
-	<div class="border hover:bg-lime-400 border-black"></div>
+	{#each plots as plot, index}
+		<button
+			class:planted={plot === 'planted'}
+			class="{selectedPlotIndex === index
+				? 'bg-lime-400'
+				: ''} hover:bg-lime-400 border border-black"
+			on:click={() => selectPlot(index)}
+		>
+			{#if plot === 'planted'}
+				<img src="/images/seeds/HeartSeed.png" alt="" />
+			{/if}
+		</button>
+	{/each}
 </div>
+
+{#if selectedPlotIndex !== null}
+	<button on:click={plantSeed} class="w-40 p-2 bg-green-600 text-white rounded-lg">
+		Plant Seed in Plot {selectedPlotIndex + 1}
+	</button>
+{/if}
+
+<style>
+	.planted {
+		background-color: #228b22;
+	}
+</style>
---
+++ src/lib/itemData.ts
@@ -0,0 +1,70 @@
+export const seeds = {
+    // create 4 more objects equivalent to heart for these categories:
+    // round, star, lumpy, square
+    Heart: {
+        name: "Heart Seed",
+        imgPath: "/images/seeds/HeartSeed.png",
+        description: "Plant these to grow a Heart Tree"
+    },
+    Round: {
+        name: "Round Seed",
+        imgPath: "/images/seeds/RoundSeed.png",
+        description: "Plant these to grow a Round Tree"
+    },
+    Star: {
+        name: "Star Seed",
+        imgPath: "/images/seeds/StarSeed.png",
+        description: "Plant these to grow a Star Tree"
+    },
+    Lumpy: {
+        name: "Lumpy Seed",
+        imgPath: "/images/seeds/LumpySeed.png",
+        description: "Plant these to grow a Lumpy Tree"
+    },
+    Square: {
+        name: "Square Seed",
+        imgPath: "/images/seeds/SquareSeed.png",
+        description: "Plant these to grow a Square Tree"
+    }
+}
+
+export const fruits = {
+    // heart - strength
+    // star - luck
+    // round - speed
+    // lumpy - stamina
+    // square - industry
+    Heart: {
+        name: "Heart Fruit",
+        imgPath: "/images/fruit/Heart.png",
+        description: "Feed this to your Bun to increase their Strength stat"
+    },
+    Star: {
+        name: "Star Fruit",
+        imgPath: "/images/fruit/Star.png",
+        description: "Feed this to your Bun to increase their Luck stat"
+    },
+    Round: {
+        name: "Round Fruit",
+        imgPath: "/images/fruit/Round.png",
+        description: "Feed this to your Bun to increase their Speed stat"
+    },
+    Lumpy: {
+        name: "Lumpy Fruit",
+        imgPath: "/images/fruit/Lumpy.png",
+        description: "Feed this to your Bun to increase their Stamina stat"
+    },
+    Square: {
+        name: "Square Fruit",
+        imgPath: "/images/fruit/Square.png",
+        description: "Feed this to your Bun to increase their Industry stat"
+    }
+}
+
+export const items = {
+    Bunzempic: {
+        name: "Bunzempic",
+        imgPath: "/images/items/Bunzempic.png",
+        description: "Use this to revive bun from the dead"
+    }
+}
