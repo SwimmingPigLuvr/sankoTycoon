d5bc7f385662de503f4f86ab4c8116d5246cc9b1
SwimmingPigLuvr <dnevell95@gmail.com>
2024-10-07 19:56:29 -0700

    It seems you didn't provide any specific changes to summarize. Could you please provide the details of the code changes you made?

---
+++ src/lib/components/Shop.svelte
@@ -0,0 +1,10 @@
+<script>
+	// add more items
+	// cycle through seeds
+	// prompt user to feed buns
+	// create array of rotating shop inventory?
+	// rotate on long interval? 2 weeks
+	// also trigger shop updates on user's progress
+</script>
+
+<main></main>
